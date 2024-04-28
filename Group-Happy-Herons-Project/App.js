58946f16e36a6183114f768958a7e95e83c12d62
jhin333 <jwen884@aucklanduni.ac.nz>
2024-04-28 18:40:48 +1200

    resolve conflicts

---
+++ Group-Happy-Herons-Project/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { ProductsList } from "./screens/ProductsList.js";
+import { ProductDetails } from "./screens/ProductDetails.js";
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Products"
+          component={ProductsList}
+          options={({ navigation }) => ({
+            title: "Products",
+            headerTitleStyle: styles.headerTitle,
+          })}
+        />
+        <Stack.Screen
+          name="ProductDetails"
+          component={ProductDetails}
+          options={({ navigation }) => ({
+            title: "Product details",
+            headerTitleStyle: styles.headerTitle,
+          })}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerTitle: {
+    fontSize: 20,
+  },
+});
+
+export default App;
