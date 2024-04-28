5bd660a253a0b5ec2a8e28c7d151c94b1efb1e33
Ze Yin <zyin737@aucklanduni.ac.nz>
2024-04-28 13:51:15 +1200

    set initial map region

--- Group-Happy-Herons-Project/screens/MapScreen.js
+++ Group-Happy-Herons-Project/screens/MapScreen.js
@@ -1,13 +1,32 @@
 import React from "react";
+import { useState } from "react";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+
 const MapScreen = () => {
+
+  const [region, setRegion] = useState({
+    latitude: -36.856331419193694,
+    latitudeDelta: 0.5496386844501089,
+    longitude: 174.7978861257434,
+    longitudeDelta: 0.5907618999481201,
+  });
+
+  const onRegionChange = (newRegion) => {
+    // console.log(newRegion);
+    setRegion(newRegion);
+  };
+
+
+
   return (
     <SafeAreaView>
       <MapView
         className="w-full h-[500px]"
         provider={PROVIDER_GOOGLE}
+        region={region}
+        onRegionChangeComplete={onRegionChange}
       ></MapView>
     </SafeAreaView>
   );
